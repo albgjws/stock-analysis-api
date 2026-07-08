70ef4bf97bf2e147cd66a0ec7d765d48c3e71f0f
ryan <253840533@qq.com>
2026-07-08 12:06:55 +0800

    chore: 删除临时脚本 fix_news_pulse.py

--- stock-analysis-app/server/services/fix_news_pulse.py
+++
@@ -1,5 +0,0 @@
-import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-import re
-
-tpl = open(r'F:\炒股训练0\炒股训练\stock-analysis-app\server\services\newsPulseService_template.txt', 'r', encoding='utf-8').read()
